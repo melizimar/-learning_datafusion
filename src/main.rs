54d6a45c9e1a40c1ee9c513483d58faefdf9dfb8
Matheus Santos <melizimar@gmail.com>
2024-10-19 12:07:33 -0300

    Primeiro exemplo vindo da documentacao

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
 use datafusion::prelude::*;
+use datafusion::arrow::util::pretty;
 
-fn main() {
-    let rt = Runtime::new();
+#[tokio::main]
+async fn main() -> datafusion::error::Result<()> {
+
+    let ctx = SessionContext::new();
+
+    let input = "./input/base-1.csv";
+
+    let opts = CsvReadOptions::new()
+        .delimiter(b';')
+        .has_header(true)
+        .file_extension("csv");
+
+    ctx.register_csv("table", input, opts).await?;
+
+    let query = "SELECT nome FROM table";
+
+    let df = ctx.sql(query).await?;
+
+    // execute the plan
+    let results = df.collect().await?;
+
+    // format the results
+    let pretty_results = pretty::pretty_format_batches(&results)?
+        .to_string();
+
+
+    println!("{}", pretty_results);
+    
+    Ok(())
 }
