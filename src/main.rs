9b74959a4316874d11f2158c9699e946b10a72a6
melizimar <melizimar@gmail.com>
2024-10-21 11:10:49 -0300

    Capturando headers

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,26 @@ async fn main() -> datafusion::error::Result<()> {
 
     let ctx = SessionContext::new();
 
-    let input = "./input/base-1.csv";
+    let input = "./input/base-50K.csv";
 
     let opts = CsvReadOptions::new()
         .delimiter(b';')
         .has_header(true)
         .file_extension("csv");
 
-    ctx.register_csv("csv_table", input, opts).await?;
+    ctx.register_csv("base", input, opts).await?;
 
     let query = r#"
         SELECT 
             *
-        FROM csv_table
+        FROM base
         LIMIT 10
     "#;
 
     let df = ctx.sql(query).await?;
+
+    // Captura os headers/cabeçalhos do arquivo csv
+    let headers = get_headers(&df);
 
     // execute the plan
     let results = df.collect().await?;
@@ -31,8 +34,21 @@ async fn main() -> datafusion::error::Result<()> {
     let pretty_results = pretty::pretty_format_batches(&results)?
         .to_string();
 
+    println!("{:?}", headers);
 
-    println!("{}", pretty_results);
-    
     Ok(())
+}
+
+fn get_headers(df: &DataFrame) -> Vec<String> {
+    // Capturar o schema (esquema) do DataFrame
+    let schema = df.schema();
+
+    // Extrair o nome das colunas para uma variável
+    let columns: Vec<String> = schema
+        .fields()
+        .iter()
+        .map(|f| f.name().clone()) // Pegar os nomes das colunas
+        .collect();
+
+    columns
 }
