eb834f7f487cbaa7dd0e1f6df5914975bb95b20a
melizimar <melizimar@gmail.com>
2024-10-22 13:25:24 -0300

    Incluindo a função get_headers um comportamento diferente

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::error::Error;
 
-use datafusion::prelude::*;
 use datafusion::arrow::util::pretty;
+use datafusion::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let input = "./input/base-50K.csv";
 
     let ctx = SessionContext::new();
-
-    let input = "./input/base-50K.csv";
 
     let opts = CsvReadOptions::new()
         .delimiter(b';')
@@ -17,31 +16,56 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     ctx.register_csv("base", input, opts).await?;
 
-    let query = r#"
-        SELECT 
-            *
-        FROM base
-        LIMIT 10
-    "#;
-
-    let df = ctx.sql(query).await?;
-
     // Captura os headers/cabeçalhos do arquivo csv
-    let headers = get_headers(&df);
+    let headers = get_headers(input).await?;
+
+    // Adiciona aspas simples em cada coluna e junta com vírgula
+    let headers_ = headers
+        .iter()
+        .map(|col| format!("\"{}\"", col)) // Adiciona aspas simples em cada coluna
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    let query_df = format!(
+        r#"
+        SELECT 
+            TRANSLATE (
+                nome,
+                ŠŽšžŸÁÇÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕËÜÏÖÑÝåáçéíóúàèìòùâêîôûãõëüïöñýÿ',
+                'SZszYACEIOUAEIOUAEIOUAOEUIONYaaceiouaeiouaeiouaoeuionyy'
+            ) as n,
+            {}
+        FROM BASE
+        LIMIT {} 
+        OFFSET {}
+    "#,
+        headers_,
+        3,  // limit = 3
+        10  // offset = 3
+    );
+
+    let df = ctx.sql(&query_df).await?;
 
     // execute the plan
     let results = df.collect().await?;
 
     // format the results
-    let pretty_results = pretty::pretty_format_batches(&results)?
-        .to_string();
+    let pretty_results = pretty::pretty_format_batches(&results)?.to_string();
 
-    println!("{:?}", headers);
-
+    println!("{}", pretty_results);
     Ok(())
 }
 
-fn get_headers(df: &DataFrame) -> Vec<String> {
+async fn get_headers(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let ctx = SessionContext::new();
+
+    let opts = CsvReadOptions::new()
+        .delimiter(b';')
+        .has_header(true)
+        .file_extension("csv");
+
+    let df = ctx.read_csv(input, opts).await?;
+
     // Capturar o schema (esquema) do DataFrame
     let schema = df.schema();
 
@@ -52,5 +76,5 @@ fn get_headers(df: &DataFrame) -> Vec<String> {
         .map(|f| f.name().clone()) // Pega os nomes das colunas
         .collect();
 
-    columns
+    Ok(columns)
 }
